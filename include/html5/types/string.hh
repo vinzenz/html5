286131a3de8262ef93b0440022c6ba0e6076bd4a
Vinzenz Feenstra <contact@virtuosic-bytes.com>
2012-03-27 09:29:10 +0200

    Using std::string for now as string

---
+++ include/html5/types/string.hh
@@ -0,0 +1,14 @@
+#ifndef GUARD_HTML5_TYPES_STRING_HH_INCLUDED
+#define GUARD_HTML5_TYPES_STRING_HH_INCLUDED
+
+#include <string>
+
+namespace html5 {
+namespace types {
+
+typedef std::string string;
+
+}}
+
+#endif //GUARD_HTML5_TYPES_STRING_HH_INCLUDED
+
