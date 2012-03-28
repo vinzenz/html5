f78c3e72d1d6a8b91dca7fd010a0dd4d75988aa9
Vinzenz Feenstra <contact@virtuosic-bytes.com>
2012-03-28 10:40:50 +0200

    DOM Document object spec

---
+++ include/html5/html/dom/document.hh
@@ -0,0 +1,46 @@
+#ifndef GUARD_HTML5_HTML_DOM_DOCUMENT_HH_INCLUDED
+#define GUARD_HTML5_HTML_DOM_DOCUMENT_HH_INCLUDED
+
+namespace html5 {
+namespace html {
+namespace dom {
+
+class document {
+public:
+	document();
+	virtual ~document();
+
+	//	W3C DOM Document Object
+	//		Properties
+	//			doctype
+	//			documentElement
+	//			documentURI
+	//			domConfig
+	//			implementation
+	//			inputEncoding
+	//			strictErrorChecking
+	//			xmlEncoding
+	//			xmlStandalone
+	//			xmlVersion
+	//		Methods
+	//			adoptNode(node)
+	//			createAttribute()
+	//			createAttributeNS()
+	//			createCDATASection()
+	//			createComment()
+	//			createDocumentFragment()
+	//			createElement()
+	//			createElementNS()
+	//			createEntityReference()
+	//			createTextNode()
+	//			createElementById()
+	//			createElementsByTagName()
+	//			createElementsByTagNameNS()
+	//			importNode()
+	//			normalizeDocument()
+	//			renameNode()
+
+}}}
+
+#endif //GUARD_HTML5_HTML_DOM_DOCUMENT_HH_INCLUDED
+
