46b518004c6d83a458bddf81690ef53a0e5e95ff
Vinzenz Feenstra <contact@virtuosic-bytes.com>
2012-03-28 10:36:42 +0200

    DOM NamedNodeMap object spec

---
+++ include/html5/html/dom/named_node_map.hh
@@ -0,0 +1,29 @@
+#ifndef GUARD_HTML5_HTML_DOM_NAMED_NODE_MAP_HH_INCLUDED
+#define GUARD_HTML5_HTML_DOM_NAMED_NODE_MAP_HH_INCLUDED
+
+namespace html5 {
+namespace html {
+namespace dom {
+
+class named_node_map {
+public:
+	named_node_map();
+	virtual ~named_node_map();
+
+	//	W3C DOM NamedNodeMap Object
+	//		Properties
+	//			length
+	//		Methods
+	//			getNamedItem()
+	//			getNamedItemNS()
+	//			item()
+	//			removeNamedItem()
+	//			removeNamedItemNS()
+	//			setNamedItem()
+	//			setNamedItemNS()
+};
+
+}}}
+
+#endif //GUARD_HTML5_HTML_DOM_NAMED_NODE_MAP_HH_INCLUDED
+
