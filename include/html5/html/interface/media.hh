6b5853d057fee0bf1e9840d02dd8d471d6016cb6
Vinzenz Feenstra <contact@virtuosic-bytes.com>
2012-03-28 20:23:32 +0200

    HTMLMediaElement definition

--- include/html5/html/interface/media.hh
+++ include/html5/html/interface/media.hh
@@ -10,13 +10,46 @@ public:
 	//	W3C HTMLMediaElement 
 	//		Properties
 	//			-- Error State
-	//			error R/O
+	//			error R/O MediaError
 	//			-- Network State
-	//			src
-	//			currentSrc R/O
+	//			src string
+	//			currentSrc R/O string
 	//			networkState R/O enum network
-	//			preload
+	//			preload string
 	//			buffered R/O TimeRanges
+	//			-- Ready State
+	//			readyState R/O uint16
+	//			seeking R/O bool
+	//			currentTime double
+	//			initialTime R/O double
+	//			duration R/O double
+	//			startOffsetTime R/O Date
+	//			defaultPlaybackRate double
+	//			playbackRate double
+	//			played R/O TimeRanges
+	//			seekable R/O TimeRanges
+	//			ended R/O bool
+	//			autoplay bool
+	//			loop bool
+	//			-- Media controller
+	//			mediaGroup
+	//			controller MediaController
+	//			-- controls
+	//			controls bool
+	//			volume double
+	//			muted bool
+	//			defaultMuted bool
+	//			-- Tracks
+	//			audioTracks R/O MultipleTrackList
+	//			videoTracks R/O ExclusiveTrackList
+	//			textTracks R/O TextTrack[]
+	//		Methods
+	//			load()
+	//			string canPlayType(string)
+	//			play()
+	//			pause()
+	//			MutableTextTrack addTextTrack(string const & kind, string const & label = string(), string const & language = string() )
+	
 
 	enum network {
 		NETWORK_EMPTY 		= 0,
