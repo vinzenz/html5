8d44b9e94e6f9e54abfb7a1fb944f3d6c8234bcc
Vinzenz Feenstra <contact@virtuosic-bytes.com>
2012-03-28 10:43:32 +0200

    DOM attr object spec

---
+++ include/html5/html/dom/attr.hh
@@ -0,0 +1,29 @@
+#ifndef GUARD_HTML5_HTML_DOM_ATTR_HH_INCLUDED
+#define GUARD_HTML5_HTML_DOM_ATTR_HH_INCLUDED
+
+namespace html5 {
+namespace html {
+namespace dom {
+
+class attr {
+public:
+	attr();
+	virtual ~attr();
+
+	//	W3C DOM Attr object
+	//		Properties
+	//			isId
+	//			name
+	//			ownerElement
+	//			schemaTypeInfo
+	//			specified
+	//			value
+	//		Methods
+	//			None
+};
+
+
+}}}
+
+#endif //GUARD_HTML5_HTML_DOM_ATTR_HH_INCLUDED
+
