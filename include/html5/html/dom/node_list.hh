9d88db653449f8158ef8a3f67da88fa19ea2097a
Vinzenz Feenstra <contact@virtuosic-bytes.com>
2012-03-28 10:31:51 +0200

    DOM NodeList object spec

---
+++ include/html5/html/dom/node_list.hh
@@ -0,0 +1,24 @@
+#ifndef GUARD_HTML5_HTML_DOM_NODE_LIST_HH_INCLUDED
+#define GUARD_HTML5_HTML_DOM_NODE_LIST_HH_INCLUDED
+
+namespace html5 {
+namespace html {
+namespace dom {
+
+class node_list {
+public:
+	node_list();
+	virtual ~node_list();
+
+	//	W3C NodeList Object
+	//		Properties
+	//			length
+	//		Methods
+	//			node item(index)
+};
+
+
+}}}
+
+#endif //GUARD_HTML5_HTML_DOM_NODE_LIST_HH_INCLUDED
+
