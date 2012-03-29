796f90495abe0cc542a32c515c42af6a2f711276
Vinzenz Feenstra <contact@virtuosic-bytes.com>
2012-03-29 08:16:14 +0200

    W3C HTMLBaseElement definition

--- include/html5/html/interface/base.hh
+++ include/html5/html/interface/base.hh
@@ -1,10 +1,22 @@
 #ifndef GUARD_HTML5_HTML_INTERFACE_BASE_HH_INCLUDED
 #define GUARD_HTML5_HTML_INTERFACE_BASE_HH_INCLUDED
 
+#include <html5/html/interface/element.hh>
+
 namespace html5 {
 namespace html {
 namespace interface {
 
+class base : public element {
+public:
+	base();
+	virtual ~base();
+
+	//	W3C HTMLBaseElement
+	//		Properties
+	//			href
+	//			target
+};
 
 }}}
 
